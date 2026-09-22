1d831170a3cf1642d04a2a0f327fa35a73b54a6e
Lukas Zavaleta <44556696+lukaszavaleta27@users.noreply.github.com>
2026-09-21 20:20:37 -0400

    Finalized ContactManager database
    Updated database with roles, disabled users, and hashed passwords

--- ContactManager.sql
+++ ContactManager.sql
@@ -74,7 +74,7 @@ CREATE TABLE `Users` (
   `IsDisabled` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Login` (`Login`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,7 +83,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (1,'John','Smith','jsmith','password123','2026-09-10 19:03:50','2026-09-15 21:19:46','Admin',0),(2,'Jane','Doe','jdoe','test123','2026-09-10 19:03:50','2026-09-15 21:20:16','User',0);
+INSERT INTO `Users` VALUES (1,'John','Smith','jsmith','$2y$12$CgCcsFBgmGAK8zmA6BAmYeCFZ.SvXJm6rSgt2hpc6iMuaQ3eUXTrK','2026-09-10 19:03:50','2026-09-22 00:15:49','Admin',0),(2,'Jane','Doe','jdoe','$2y$12$CH4LhPpm6b.LJzhiURWA6.MOa1C.fXI4EHQ1/gL7movQtikFB95U2','2026-09-10 19:03:50','2026-09-22 00:16:43','User',0),(23,'Application','Administrator','root','$2y$12$plTrEQyr2wM2U8O8V0iUz.HSt.8TslDZXXZ05150evZunMdxi1zO.','2026-09-22 00:01:04','2026-09-22 00:03:39','Admin',0);
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -96,4 +96,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-09-15 21:24:45
+-- Dump completed on 2026-09-22  0:17:47
